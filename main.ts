9d9d2fd74eeac01f887f1076bb1e2c6b0f81d56f
Mayravh71 <106397591+Mayravh71@users.noreply.github.com>
2025-02-11 23:26:58 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+player.onChat("TierraAgua", function () {
+    player.teleport(world(704, 47, -408))
+})
+player.onChat("TierraNieve", function () {
+    player.teleport(world(-338, 68, 365))
+})
+player.onChat("TierraVerde", function () {
+    player.teleport(world(126, 70, -15))
+})
